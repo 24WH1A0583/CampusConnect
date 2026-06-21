77ee17756ad572cc89b4a962c94ec8864ce0e0cd
charvi <24wh1a0583@bvrithyderabad.edu.in>
2026-06-21 18:32:36 +0530

    base url change

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5001/api",
+ baseURL:
+"https://your-backend-name.onrender.com/api",
 });
 
 export default api;
